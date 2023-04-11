455edccab167159b46acbfa55a0b84aa00f78100
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-04-11 15:59:00 -0300

    feat: adding model prediction

--- ml_drf_api/core/tests.py
+++ ml_drf_api/core/tests.py
@@ -4,13 +4,48 @@ from django.urls import reverse
 
 class TestCoreView:
     def test_infer_view_auth(self, auth_user_client):
+        data = {
+            "input": [
+                17.99,
+                10.38,
+                122.8,
+                1001,
+                0.1184,
+                0.2776,
+                0.3001,
+                0.1471,
+                0.2419,
+                0.07871,
+                1.095,
+                0.9053,
+                8.589,
+                153.4,
+                0.006399,
+                0.04904,
+                0.05373,
+                0.01587,
+                0.03003,
+                0.006193,
+                25.38,
+                17.33,
+                184.6,
+                2019,
+                0.1622,
+                0.6656,
+                0.7119,
+                0.2654,
+                0.4601,
+                0.1189,
+            ]
+        }
         url = reverse("infer")
-        response = auth_user_client.get(url)
-        assert response.json()["message"] == "This is the model's prediction:"
+        response = auth_user_client.post(url, data)
+        assert response.json()["message"] == "This is the model's prediction: 1"
         assert response.status_code == 200
 
     def test_infer_view_unauth(self, unauth_user_client):
+        data = {"input": ""}
         url = reverse("infer")
-        response = unauth_user_client.get(url)
+        response = unauth_user_client.post(url, data)
         assert isinstance(response, HttpResponseForbidden)
         assert response.status_code == 403
--- ml_drf_api/core/views.py
+++ ml_drf_api/core/views.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
+import joblib
+from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 
-# from django.shortcuts import render
+model_path = Path(settings.APPS_DIR / "ml/saved_models/randomforest_breast_cancer.pkl")
 
 
 def infer(request):
     if not request.user.is_authenticated:
         raise PermissionDenied
     else:
-        payload = {"message": "This is the model's prediction:"}
+        estimator = joblib.load(model_path)
+        prediction = estimator.predict([request.POST.getlist("input")])
+
+        payload = {"message": f"This is the model's prediction: {prediction[0]}"}
         return JsonResponse(payload)
