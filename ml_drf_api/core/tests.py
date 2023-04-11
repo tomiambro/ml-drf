2093e2926cff87f0df568659399b599c0023fbff
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-04-11 14:52:37 -0300

    feat: adding more tests

--- ml_drf_api/core/tests.py
+++ ml_drf_api/core/tests.py
@@ -1,11 +1,18 @@
-# from django.test import TestCase
+from django import test
+from django.http.response import HttpResponseForbidden
 from django.urls import reverse
-
-# Create your tests here.
 
 
 class TestCoreView:
-    def test_infer_view(self, user_client):
+    def test_infer_view_auth(self, user_client):
         url = reverse("infer")
         response = user_client.get(url)
         assert response.json()["message"] == "This is the model's prediction:"
+        assert response.status_code == 200
+
+    def test_infer_view_unauth(self, user):
+        url = reverse("infer")
+        client = test.Client()
+        response = client.get(url)
+        assert isinstance(response, HttpResponseForbidden)
+        assert response.status_code == 403
--- ml_drf_api/core/views.py
+++ ml_drf_api/core/views.py
@@ -1,11 +1,12 @@
-from django.http import Http404, JsonResponse
+from django.core.exceptions import PermissionDenied
+from django.http import JsonResponse
 
 # from django.shortcuts import render
 
 
 def infer(request):
-    if request.user.is_authenticated:
+    if not request.user.is_authenticated:
+        raise PermissionDenied
+    else:
         payload = {"message": "This is the model's prediction:"}
         return JsonResponse(payload)
-    else:
-        raise Http404
