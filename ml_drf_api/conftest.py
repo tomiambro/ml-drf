9c74bda3e1164425b230b8d0cfe42e6a32794ffe
tomiambro <42741525+tomiambro@users.noreply.github.com>
2023-04-11 16:17:30 -0300

    feat: adding train command and dataset (#10)
    * feat: adding train command and dataset

--- ml_drf_api/conftest.py
+++ ml_drf_api/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from django import test
 
 from ml_drf_api.users.models import User
 from ml_drf_api.users.tests.factories import UserFactory
@@ -12,3 +13,16 @@ def media_storage(settings, tmpdir):
 @pytest.fixture
 def user(db) -> User:
     return UserFactory()
+
+
+@pytest.fixture
+def auth_user_client(user):
+    client = test.Client()
+    client.force_login(user)
+    return client
+
+
+@pytest.fixture
+def unauth_user_client(user):
+    client = test.Client()
+    return client
--- ml_drf_api/core/views.py
+++ ml_drf_api/core/views.py
@@ -1,11 +1,19 @@
-from django.http import Http404, JsonResponse
+from pathlib import Path
 
-# from django.shortcuts import render
+import joblib
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+from django.http import JsonResponse
+
+model_path = Path(settings.APPS_DIR / "ml/saved_models/randomforest_breast_cancer.pkl")
 
 
 def infer(request):
-    if request.user.is_authenticated:
-        payload = {"message": "This is the model's prediction:"}
-        return JsonResponse(payload)
+    if not request.user.is_authenticated:
+        raise PermissionDenied
     else:
-        raise Http404
+        estimator = joblib.load(model_path)
+        prediction = estimator.predict([request.POST.getlist("input")])
+
+        payload = {"message": f"This is the model's prediction: {prediction[0]}"}
+        return JsonResponse(payload)
