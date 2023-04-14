3f7e9b9508508c648b68959d9806ef2b00be958d
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-04-14 16:47:40 -0300

    feat: adding desc to admin

--- ml_drf_api/core/admin.py
+++ ml_drf_api/core/admin.py
@@ -5,7 +5,7 @@ from .models import ModelLog
 
 @admin.register(ModelLog)
 class ModelLogAdmin(admin.ModelAdmin):
-    list_display = ("name", "output", "error_log", "status")
+    list_display = ("name", "output", "error_log", "description")
     readonly_fields = ("input", "output", "error_log", "status")
 
     def name(self, obj):
