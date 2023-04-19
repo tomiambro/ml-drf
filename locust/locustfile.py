93544e4bcb2b6d45cb7dd3b8b881532f7b232b41
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-04-18 23:42:34 -0300

    feat: adding register endpoint for creating new users

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,3 +1,4 @@
+import logging
 import random
 
 from locust import HttpUser, task
@@ -36,7 +37,29 @@ input_data = [
 ]
 
 
-class InferUser(HttpUser):
+class WebUser(HttpUser):
+    user_data = {"username": "locust", "password": "locust"}
+
+    def on_start(self):
+        # Create a new user
+        response = self.client.post("/api/users/register/", self.user_data)
+        if response.status_code == 201:
+            logging.info("User created successfully")
+        else:
+            logging.error(response.json())
+            raise Exception("Cannot create user")
+
+        # Perform authentication
+        response = self.client.post("/auth-token/", self.user_data)
+        if response.status_code == 200:
+            # Save the token in the session
+            self.client.headers["Authorization"] = "Token " + response.json()["token"]
+            logging.info("User authenticated successfully")
+        else:
+            # Authentication failed, raise an exception
+            logging.error(response.json())
+            raise Exception("Failed to authenticate")
+
     @task
     def test_home_page(self):
         self.client.get("/")
@@ -49,15 +72,11 @@ class InferUser(HttpUser):
         res = self.client.post("/core/infer", data)
         assert res.status_code == 200
 
-    def on_start(self):
-        # Perform authentication
-        response = self.client.post(
-            "/auth-token/", {"username": "locust", "password": "locust"}
-        )
-        if response.status_code == 200:
-            # Save the token in the session
-            self.client.headers["Authorization"] = "Token " + response.json()["token"]
+    def on_stop(self):
+        # Delete the recently created user
+        response = self.client.delete("/api/users/locust/")
+        if response.status_code == 204:
+            logging.info("User deleted successfully")
         else:
-            # Authentication failed, raise an exception
-            print(response)
-            raise Exception("Failed to authenticate")
+            logging.error(response.json())
+            raise Exception("Cannot delete user")
--- ml_drf_api/users/api/views.py
+++ ml_drf_api/users/api/views.py
@@ -1,16 +1,28 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import (
+    DestroyModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+)
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from .serializers import UserSerializer
+from .serializers import CreateUserSerializer, UserSerializer
 
 User = get_user_model()
 
 
-class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(
+    RetrieveModelMixin,
+    ListModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+    GenericViewSet,
+):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = "username"
@@ -23,3 +35,21 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     def me(self, request):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    # Register a new user
+    @action(detail=False, methods=["post"], permission_classes=[AllowAny])
+    def register(self, request):
+        serializer = CreateUserSerializer(
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            # serializer.save()
+            User.objects.create_user(
+                username=serializer.validated_data["username"],
+                email=serializer.validated_data.get("email", ""),
+                password=serializer.validated_data["password"],
+            )
+            return Response(
+                status=status.HTTP_201_CREATED, data=serializer.validated_data
+            )
+        return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
