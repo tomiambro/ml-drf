7d2eb588f10cb37b52cedbc8fbe113557f209437
tomiambro <42741525+tomiambro@users.noreply.github.com>
2023-04-19 15:45:05 -0300

    test: adding user tests (#20)
    * test: adding user tests
    
    * test: removing breakpoint

--- ml_drf_api/users/tests/test_drf_views.py
+++ ml_drf_api/users/tests/test_drf_views.py
@@ -1,9 +1,11 @@
 import pytest
+from django import test
 from django.urls import reverse
 from rest_framework.test import APIRequestFactory
 
 from ml_drf_api.users.api.views import UserViewSet
 from ml_drf_api.users.models import User
+from ml_drf_api.users.tests.factories import UserFactory
 
 
 class TestUserViewSet:
@@ -34,6 +36,26 @@ class TestUserViewSet:
             "name": user.name,
             "url": f"http://testserver/api/users/{user.username}/",
         }
+
+    def test_list_users(self, auth_user_client):
+        UserFactory()
+        url = reverse("api:user-list")
+        response = auth_user_client.get(url)
+
+        assert response.status_code == 200, response.json()
+        assert len(response.json()) == 1
+
+    def test_delete_user(self, user):
+        # User 2 tries to delete another user
+        user_2 = UserFactory()
+        client = test.Client()
+        client.force_login(user_2)
+        url = reverse("api:user-detail", kwargs={"username": user.username})
+
+        response = client.delete(url)
+        assert response.status_code == 404, response.json()
+        # Assert original user still exists
+        assert User.objects.filter(username=user.username).exists()
 
     def test_register(self, unauth_user_client):
         url = reverse("api:user-register")
